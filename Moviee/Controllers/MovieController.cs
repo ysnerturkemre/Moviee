8ab05cd5ea673f8b7d0a09152ee79472ca62e11e
ysnerturkemre <yasin_emre_96@msn.com>
2021-01-02 15:45:10 +0300

    commit

---
+++ Moviee/Controllers/MovieController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Moviee.Models;
+
+namespace Moviee.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        DbMovieEntities db = new DbMovieEntities();
+        public ActionResult Index()
+        {
+
+            var movies = db.TblMovie.ToList();
+            return View(movies);
+        }
+
+        [HttpGet]
+        public ActionResult Movieadd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Movieadd(TblMovie t)
+        {
+            db.TblMovie.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MovieDelete(int id)
+        {
+            var movie = db.TblMovie.Find(id);
+            db.TblMovie.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult MovieUpdate(int id)
+        {
+            var mov = db.TblMovie.Find(id);
+            return View("MovieUpdate", mov);
+        }
+        public ActionResult MovieUpdatee(TblMovie t)
+        {
+            var movie = db.TblMovie.Find(t.MovieId);
+            movie.Name = t.Name;
+            movie.Director = t.Director;
+            movie.MovieYear = t.MovieYear;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Moviee/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Moviee.MvcApplication" Language="C#" %>
